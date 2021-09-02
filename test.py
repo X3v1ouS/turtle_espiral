8c30c38ef56c26de30bea4909b218fd47721fff0
X3v1ouS <ernestroda@gmail.com>
2021-09-02 23:09:40 +0200

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+import turtle
+
+
+colors = ["red", "white", "blue", "green", "yellow", "orange"]
+
+pes = turtle.Pen()
+pes.speed(5)
+turtle.bgcolor("black")
+
+for x in range(360):
+    pes.pencolor(colors[x%len(colors)])
+    pes.width(x/100+1)
+    pes.forward(x)
+    pes.left(59)
+
